bdeb5311961e8ce9f4651bb5855f415d34b482a4
Shane <shaneor1@hotmail.com>
2021-07-02 17:23:00 +0100

    adding data to csv file -County location lat and long- and tested second json file

--- Main.py
+++ Main.py
@@ -9,12 +9,17 @@ import plotly.graph_objs as go
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 from plotly import __version__
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
-f = open('C:\\Users\\Shane\\proj\\Google Maps\\Takeout\All\\2016_NOVEMBER.json')
+import datetime as dt
+from datetime import date
+f = open('C:\\Users\\Shane\\proj\\Google Maps\\Takeout\All\\2019_NOVEMBER.json')
 data = json.load(f)
-p=open('C:\\Users\\Shane\\proj\\Google Maps\\CSV_1','w')
+p=open('C:\\Users\\Shane\\proj\\Google Maps\\CSV_1.csv','w')
 
 Counties = ['Carlow', 'Cavan', 'Clare', 'Cork', 'Donegal', 'Dublin', 'Galway', 'Kerry', 'Kildare', 'Kilkenny', 'Laois', 'Leitrim', 'Limerick', 'Longford', 'Louth', 'Mayo', 'Meath', 'Monaghan', 'Offaly', 'Roscommon', 'Sligo', 'Tipperary', 'Waterford', 'Westmeath', 'Wexford', 'Wicklow']
 
+
+rows=[]
+d={}
 # CREATE THE CSV FILE from json file
 count=0
 for i in data['timelineObjects']:
@@ -24,18 +29,36 @@ for i in data['timelineObjects']:
         lat = str(data['timelineObjects'][count]['activitySegment']['endLocation']['latitudeE7'])
         longi = str(data['timelineObjects'][count]['activitySegment']['endLocation']['longitudeE7'])
         print(str(count)+ ' '+'Latitude = '+lat+' ' + 'Longitude = '+longi)
-        row = [str(count),str(lat),str(longi)]
-        p.writelines(row)
+        #row = [str(count),str(lat),str(longi)]
+        d={}
+        d['Num']= str(count)
+        d['latitude']=lat
+        d['Longitude']=longi
+        
+        
+        
+
+        
+       
     else : 
         adr = str(data['timelineObjects'][count]['placeVisit']['location']['address'])
         for i in Counties:
             if (i in adr):
-                print(i)
                 adr = adr.replace("\n", "\t")
                 adr = adr.strip()
                 print(str(count)+ ' '+adr)
                 row2=[str(count),adr]
-                p.writelines(row2)
+                p.write(str(count))
+                p.write(adr)
+                #p.writelines(row2)
+                d['Num2']= str(count)
+                d['Address']=adr
+                d['County']=i
+                rows.append(d)
+                dfGoogleStats = pd.DataFrame(rows)
+                today = date.today()
+                d1 = today.strftime("%d_%m_%Y")
+                dfGoogleStats.to_csv(f'C:\\Users\\Shane\\proj\\Google Maps\\GoogleMaps_{d1}.csv', index=False)
     count=count+1
 
 #data = dict(type = 'choropleth',
@@ -57,5 +80,5 @@ for i in data['timelineObjects']:
     
 
    # print('location = '+adr)
-
+p.close()
 f.close()
